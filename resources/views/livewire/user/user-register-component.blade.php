73fca4b0a4d270e29265ce67389aaa9ef4de0287
babamurad <babamurad2010@yandex.ru>
2024-07-03 16:19:54 +0500

    delete register method in user-register-component

--- resources/views/livewire/user/user-register-component.blade.php
+++ resources/views/livewire/user/user-register-component.blade.php
@@ -33,51 +33,51 @@
 
                         <div class="card-footer">
                             <button type="submit" class="btn btn-primary" wire:click.prevent="login">войти</button>
-                            <a href="" wire:click.prevent="regLog">Регистрация нового сотрудника</a>
+{{--                            <a href="" wire:click.prevent="regLog">Регистрация нового сотрудника</a>--}}
                         </div>
                     </form>
                 </div>
-                @else
-                    <div class="card card-primary my-5">
-                        <div class="card-header">
-                            <h3 class="card-title">Регистрация</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <!-- form start -->
-                        <form>
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Your name" wire:model="name">
-                                    @error('name') <span class="text-danger">{{ $message }}</span> @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1"
-                                           placeholder="Enter email" wire:model="email">
-                                    @error('email') <span class="text-danger">{{ $message }}</span> @enderror
-                                </div>
+{{--                @else--}}
+{{--                    <div class="card card-primary my-5">--}}
+{{--                        <div class="card-header">--}}
+{{--                            <h3 class="card-title">Регистрация</h3>--}}
+{{--                        </div>--}}
+{{--                        <!-- /.card-header -->--}}
+{{--                        <!-- form start -->--}}
+{{--                        <form>--}}
+{{--                            <div class="card-body">--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="name">Name</label>--}}
+{{--                                    <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Your name" wire:model="name">--}}
+{{--                                    @error('name') <span class="text-danger">{{ $message }}</span> @enderror--}}
+{{--                                </div>--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="exampleInputEmail1">Email address</label>--}}
+{{--                                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail1"--}}
+{{--                                           placeholder="Enter email" wire:model="email">--}}
+{{--                                    @error('email') <span class="text-danger">{{ $message }}</span> @enderror--}}
+{{--                                </div>--}}
 
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword1"
-                                           placeholder="Password" wire:model="password">
-                                    @error('password') <span class="text-danger">{{ $message }}</span> @enderror
-                                </div>
-                                <div class="form-group">
-                                    <label for="password_confirmation">Confirm Password:</label>
-                                    <input type="password" id="password_confirmation"  class="form-control" wire:model="password_confirmation" placeholder="Confirm Password">
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="exampleInputPassword1">Password</label>--}}
+{{--                                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword1"--}}
+{{--                                           placeholder="Password" wire:model="password">--}}
+{{--                                    @error('password') <span class="text-danger">{{ $message }}</span> @enderror--}}
+{{--                                </div>--}}
+{{--                                <div class="form-group">--}}
+{{--                                    <label for="password_confirmation">Confirm Password:</label>--}}
+{{--                                    <input type="password" id="password_confirmation"  class="form-control" wire:model="password_confirmation" placeholder="Confirm Password">--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
+{{--                            <!-- /.card-body -->--}}
 
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary" wire:click.prevent="register">Регистрация</button>
-                                <a href="" wire:click.prevent="regLog">Уже зарегистрированы?</a>
-                                <button type="submit" class="btn btn-primary float-sm-right" wire:click.prevent="logout">Logout</button>
-                            </div>
-                        </form>
-                    </div>
+{{--                            <div class="card-footer">--}}
+{{--                                <button type="submit" class="btn btn-primary" wire:click.prevent="register">Регистрация</button>--}}
+{{--                                <a href="" wire:click.prevent="regLog">Уже зарегистрированы?</a>--}}
+{{--                                <button type="submit" class="btn btn-primary float-sm-right" wire:click.prevent="logout">Logout</button>--}}
+{{--                            </div>--}}
+{{--                        </form>--}}
+{{--                    </div>--}}
                 @endif
             </div>
         </div>
